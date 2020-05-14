412e9248ca56767679cb834b515c093e561a34a3
Ernesto <e.ernesto.espinosa@gmail.com>
2020-05-14 18:23:31 -0500

    [5] Integrate logic to push to Prometheus into the Monitor class

--- lib/collector.rb
+++ lib/collector.rb
@@ -7,18 +7,6 @@
 # https://github.com/obfuscurity/synthesize. Please do not use an agent such as Telegraf or
 # Collectd. We would like to see how you would code this :)
 
-require_relative 'command_executor'
 require_relative 'monitor'
 
-include CommandExecutor
-
-osname = execute('uname')
-monitor = Monitor.new(osname)
-
-PUSHGATEWAY_HOST = ENV['PUSHGATEWAY_HOST']
-PUSHER_COMMAND = "echo \"%{data}\" | curl --data-binary @- #{PUSHGATEWAY_HOST}/metrics/job/%{pid}/instance/#{monitor.hostname}"
-
-DATA_TEMPLATE = "# TYPE cpu_usage gauge\n" \
-                "memory_usage_%{pid}{label=\"Memory Usage\",command=\"%{command}\",instance=\"%{instance}\",pid=\"%{pid}\"} %{memory_usage}\n"
-
-puts monitor.collect
+Monitor.new.start
--- lib/monitor.rb
+++ lib/monitor.rb
@@ -1,10 +1,32 @@
 # frozen_string_literal: true
 
+require_relative 'command_executor'
 require_relative 'os_strategies'
 
 class Monitor
-  def initialize(osname)
+  include CommandExecutor
+
+  PUSHGATEWAY_HOST = ENV['PUSHGATEWAY_HOST']
+  PUSHER_COMMAND = "echo \"%{data}\" | curl --data-binary @- #{PUSHGATEWAY_HOST}/metrics/job/%{pid}/instance/%{instance}"
+
+  DATA_TEMPLATE = "# TYPE cpu_usage gauge\n" \
+                  "memory_usage_%{pid}{label=\"Memory Usage\",command=\"%{command}\",instance=\"%{instance}\",pid=\"%{pid}\"} %{memory_usage}\n"
+
+  def initialize(delay = 10)
+    osname = execute('uname')
+
+    @delay = delay
     @strategy = OSStrategies.for(osname)
+  end
+
+  def start
+    loop do
+      collect
+        .map { |entry| { data: DATA_TEMPLATE % entry, pid: entry[:pid], instance: entry[:instance] } }
+        .each { |message| (PUSHER_COMMAND % message).tap(&method(:puts)).tap { |command| execute(command) } }
+
+      sleep(@delay)
+    end
   end
 
   # Gives a list of hash with :pid, :memused, :command.
